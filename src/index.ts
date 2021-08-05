2a6e79c959b086f6f3acbbfb896d40ff55f24117
Gerrit Rindermann <Gerrit.Rindermann@Gmail.com>
2021-08-04 17:54:41 -0700

    Export Yopto Scripts to use in nextJs script tag

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import YotpoStarRating from './components/YotpoStarRating/YotpoStarRating';
 import YotpoReviews from './components/YotpoReviews/YotpoReviews';
 import YotpoScripts from './components/YotpoScripts/YotpoScripts';
+import { YOTPO_SCRIPTS } from './components/YotpoScripts/YotpoScripts';
 import { useYotpoRefresh } from './hooks/useYotpoRefresh';
 
-export { YotpoStarRating, YotpoReviews, useYotpoRefresh, YotpoScripts };
+export { YotpoStarRating, YotpoReviews, useYotpoRefresh, YotpoScripts, YOTPO_SCRIPTS };
